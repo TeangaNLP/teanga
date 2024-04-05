6c680e3c4b6adc26936fd36bb0970865b95850c7
John P. McCrae <john@mccr.ae>
2024-04-05 17:18:36 +0100

    Remove hack

--- teanga/__init__.py
+++ teanga/__init__.py
@@ -1,4 +1,4 @@
-from .corpus import Corpus, read_yaml, read_json, read_yaml_str, read_json_str, try_load_teanga_pyo3
+from .corpus import Corpus, read_yaml, read_json, read_yaml_str, read_json_str
 from .document import Document
 from .service import Service, rest_service
 from .layer_desc import LayerDesc
