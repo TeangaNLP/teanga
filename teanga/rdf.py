66e967bb7330cfee08d361749f865f1ac9771423
John P. McCrae <john@mccr.ae>
2024-05-21 13:40:52 +0100

    Work on RDF conversion

--- teanga/rdf.py
+++ teanga/rdf.py
@@ -1,5 +1,6 @@
 import rdflib
 from rdflib import RDF
+import teanga
 
 TEANGA_BUILT_INS = set(["text", "words", "sentences", "paragraphs"])
 
@@ -22,6 +23,7 @@ def teanga_corpus_to_rdf(graph, corpus, url: str):
         graph.add((rdflib.URIRef(doc_url), RDF.type, teanga.Document))
         graph.add((rdflib.URIRef(url), teanga.document, rdflib.URIRef(doc_url)))
         for layer in document.layers:
+            print(layer)
             layer_desc = corpus.get_meta()[layer]
             if "uri" in layer_desc.meta:
                 layer_url = rdflib.URIRef(layer_desc.meta["uri"])
@@ -33,28 +35,70 @@ def teanga_corpus_to_rdf(graph, corpus, url: str):
                 graph.add((rdflib.URIRef(doc_url), 
                            layer_url,
                            rdflib.Literal(document.get_layer(layer).text(document))))
-            elif layer_desc.layer_type == "span":
+            else:
                 for idx, data in enumerate(document.get_layer(layer).raw()):
-                    node_url = (url + "#" + document.id + "&layer=" 
-                                + layer + "&idx=" + str(idx))
+                    base_layer = layer_desc.base
+                    base_layer_desc = corpus.get_meta()[base_layer]
+                    node_url = _node_url(url, document.id, layer,
+                                         layer_desc.layer_type, idx)
                     node = rdflib.URIRef(node_url)
                     graph.add((rdflib.URIRef(doc_url), layer_url, node))
                     graph.add((node, teanga.idx, rdflib.Literal(idx)))
-                    write_teanga_data(graph, node, data, teanga, 
-                                      layer_desc.data, layer_desc.values)
+                    if layer_desc.layer_type == "elements":
+                        target_url = _node_url(url, document.id, base_layer,
+                                               base_layer_desc.layer_type, data[0])
+                        data_value = data[1]
+                    elif layer_desc.layer_type == "seq":
+                        target_url = _node_url(url, document.id, base_layer,
+                                               base_layer_desc.layer_type, idx)
+                        data_value = data
+                    elif layer_desc.layer_type == "span":
+                        target_url = _node_url(url, document.id, base_layer,
+                                               base_layer_desc.layer_type, data[0], 
+                                               data[1])
+                        data_value = data[2]
+                    elif layer_desc.layer_type == "div":
+                        target_url = _node_url(url, document.id, base_layer,
+                                               base_layer_desc.layer_type, data[0])
+                        data_value = data[1]
+                    graph.add((node, teanga.ref, rdflib.URIRef(target_url)))
+                    write_teanga_data(graph, node, data_value, teanga, 
+                                      layer_desc.data, 
+                                      layer_desc.values,
+                                      url, document)
 
     return graph
+
+def _node_url(url: str, doc_id : str, layer: str, 
+              layer_type: str, idx: int, end_idx: int = None):
+    if layer_type == "characters":
+        if end_idx:
+            return (url + "#" + doc_id + "&layer=" + layer + 
+                    "&char=" + str(idx) + "," + str(end_idx))
+        else:
+            return url + "#" + doc_id + "&layer=" + layer + "&char=" + str(idx)
+    else:
+        return url + "#" + doc_id + "&layer=" + layer + "&idx=" + idx
     
-def write_teanga_data(graph, node, data, teanga, layer_desc):
+def write_teanga_data(graph : rdflib.Graph, 
+                      node : rdflib.URIRef, 
+                      data, 
+                      teanga : rdflib.Namespace, 
+                      layer_desc : teanga.corpus.LayerDesc,
+                      url : str,
+                      document: teanga.Document) -> None:
     data_type = layer_desc.layer_type
     if data_type is None:
         return
     elif data_type == "string":
         graph.add((node, teanga.data, rdflib.Literal(data)))
     elif data_type == "link":
-        # TODO:
-        return
-        
-    
+        target_url = _node_url(url, document.id, layer_desc.target,
+                               document.get_meta()[layer_desc.target].layer_type,
+                               data)
 
-    
+        graph.add((node, teanga.link, rdflib.URIRef(target_url)))
+    elif isinstance(data_type, list):
+        graph.add((node, teanga.data, rdflib.Literal(data)))
+    else:
+        raise ValueError("Unknown data type: " + data_type)
--- tests/test_rdf.py
+++ tests/test_rdf.py
@@ -2,10 +2,35 @@ import rdflib
 import teanga.rdf as rdf
 import teanga
 
-def test_teanga_corpus_to_rdf():
+def test_teanga_corpus_to_rdf_1():
     corpus = teanga.Corpus()
     corpus.add_layer_meta("text", layer_type="characters")
     corpus.add_doc(text="Hello")
     graph = rdflib.Graph()
     rdf.teanga_corpus_to_rdf(graph, corpus, "http://example.org/corpus")
     assert len(graph) == 4
+
+def test_teanga_corpus_to_rdf_2():
+    corpus = teanga.Corpus()
+    corpus.add_layer_meta("text", layer_type="characters")
+    corpus.add_layer_meta("words", layer_type="span", base="text")
+    doc = corpus.add_doc("Hello there! Goodbye!")
+    doc.words = [(0, 5), (6, 12), (14, 22)]
+    print(doc.layers)
+    graph = rdflib.Graph()
+    rdf.teanga_corpus_to_rdf(graph, corpus, "http://example.org/corpus")
+    for s, p, o in graph:
+        print(s, p, o)
+    assert ((rdflib.URIRef("http://example.org/corpus#xQAb"), 
+             rdflib.URIRef("http://teanga.io/teanga#text"), 
+             rdflib.Literal("Hello there! Goodbye!")) in graph)
+    assert ((rdflib.URIRef("http://example.org/corpus#xQAb"),
+             rdflib.URIRef("http://teanga.io/teanga#words"),
+             rdflib.URIRef("http://example.org/corpus#xQAb&layer=words&idx=0")) in graph)
+    assert ((rdflib.URIRef("http://example.org/corpus#xQAb&layer=words&idx=0"),
+             rdflib.URIRef("http://teanga.io/teanga#idx"),
+             rdflib.Literal(0)) in graph)
+    assert ((rdflib.URIRef("http://example.org/corpus#xQAb&layer=words&idx=0"),
+             rdflib.URIRef("http://teanga.io/teanga#ref"),
+             rdflib.URIRef("http://example.org/corpus#xQAb&layer=text&chars=0,5")) in graph)
+    
