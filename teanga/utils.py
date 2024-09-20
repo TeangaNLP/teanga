c199493650eefa0e4d036780ae4432610bca9cb4
bernardo <bernardo.stearns@gmail.com>
2024-09-20 13:56:41 +0100

    adding utils docs

--- teanga/utils.py
+++ teanga/utils.py
@@ -6,20 +6,18 @@ def teanga_id_for_doc(ids, *args, **kwargs):
     """Return the Teanga ID for a document.
 
     Parameters:
-    -----------
-    ids: str
-        The IDs already generated and not to be repeated
+        ids: str
+            The IDs already generated and not to be repeated
 
-    This works as the add_doc method, but returns the Teanga ID for the document.
-    It is not necessary to call this method directly but instead you can use it
-    via the Corpus class.
+        This works as the add_doc method, but returns the Teanga ID for the document.
+        It is not necessary to call this method directly but instead you can use it
+        via the Corpus class.
 
     Examples:
-    ---------
-    >>> teanga_id_for_doc(set(), text="This is a document.")
-    'Kjco'
-    >>> teanga_id_for_doc(set(), en="This is a document.", nl="Dit is een document.")
-    'Nnrd'
+        >>> teanga_id_for_doc(set(), text="This is a document.")
+        'Kjco'
+        >>> teanga_id_for_doc(set(), en="This is a document.", nl="Dit is een document.")
+        'Nnrd'
     """
     text = ""
     if len(kwargs) == 0:
