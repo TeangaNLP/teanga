74906bd94cad7a4d93baa48e2b27263486d18cde
priya-74llh <priya74_llh@jnu.ac.in>
2024-11-01 10:56:08 +0000

    latest changes

--- conlu_corpus/conllu2yaml_function_example.py
+++ conlu_corpus/conllu2yaml_function_example.py
@@ -6,3 +6,5 @@ import conllu2yamlnew2
 
 conllu2yamlnew2.convert_all_files(['sga_dipsgg-ud-test.conllu', 'ga_twittirish-ud-train.conllu'])
 
+# file1: /home/priya/teanga2/conlu_corpus/sga_dipsgg-ud-test.conllu
+# file2: /home/priya/teanga2/conlu_corpus/ga_twittirish-ud-train.conllu
